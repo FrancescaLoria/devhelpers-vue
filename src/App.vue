36024b513c52590b7cbc11b6e463c48b15a5a897
Massimiliano-Novello <massinov5@gmail.com>
2023-07-07 11:09:20 +0200

    developer card

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
-
 <script>
-
+import DeveloperCard from "./components/DeveloperCard.vue";
+export default {
+  components: {
+    DeveloperCard,
+  },
+};
 </script>
 
 <template>
   <h2 class="m-4">HELLO VITA DE MERD</h2>
   <h5>ciao sono Paola</h5>
+  <DeveloperCard />
 </template>
-
-
 
 <style lang="scss">
 @use "./styles/general.scss" as *;
---
+++ src/components/DeveloperCard.vue
@@ -0,0 +1,39 @@
+<script>
+import axios from 'axios';
+export default {
+    name: 'developerCard',
+    data() {
+        return {
+            developers: []
+        }
+    },
+    mounted () {
+        this.getDeveloper()
+    },
+    methods: {
+        getDeveloper() {
+            axios.get("http://localhost:8000/api/developers").then((resp) => {
+                console.log(resp);
+                this.developers = resp.data.results
+            })
+        }
+    }
+}
+</script>
+
+
+<template>
+
+<div class="card" v-for="developer in developers">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">{{ developer.name }}</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+</template>
+
+<style>
+</style>
