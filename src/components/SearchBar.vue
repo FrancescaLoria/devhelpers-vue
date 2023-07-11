80993faf4b60d7ad4a3b8b233d21e2707d4aefcf
Angelop26 <angeloprospero26@gmail.com>
2023-07-11 17:06:35 +0200

    add comment filter

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -3,12 +3,14 @@ import axios from 'axios';
 import { store } from "../store";
 import DeveloperCard from "./DeveloperCard.vue";
 import Filters from './Filters.vue';
+import filterComment from './filterComment.vue';
 
 export default {
   name: "SearchBar",
   components: {
     DeveloperCard,
     Filters,
+    filterComment
   },
   data() {
     return {
@@ -18,6 +20,8 @@ export default {
       developersByVote: [], // Developers filtrati in base alla media voto (>=)
       ids: [],  // Array di id
       idsString: "",  // Sringa con id collegati con &
+      developersByComment: [], // Developers filtrati in base al numero di recensioni (>=)
+      
     };
   },
   mounted() {
@@ -59,19 +63,34 @@ export default {
         this.developersByVote = this.developers;
       }
     },
+    getFilterComment(comment) {
+      console.log("Commento", comment);
+      // Non funziona bene
+      if (comment != '') {
+        axios.get(`${this.store.apiUrl}/api/review/` + comment)
+          .then((resp) => {
+            this.developersByComment = resp.data.results;
+            console.log("By comment", this.developersByComment);
+          })
+      } else {
+        this.developersByComment = this.developers;
+      }
+    },
   },
   computed: {
     filteredDevelopers() {
       // Non funziona bene
-      if (this.developersByVote != '') {
+      if (this.developersByVote != '' || this.developersByComment != '') {
         let filteredDevelopers = [];
         this.developersByVote.forEach(elem => {
           this.developers.forEach(dev => {
-            if (dev.id === elem.id) {
-              filteredDevelopers.push(dev);
-              console.log(filteredDevelopers);
-            }
-          });
+            this.developersByComment.forEach(comment =>{
+              if (dev.id === elem.id && elem.id === comment.id) {
+                filteredDevelopers.push(dev);
+                console.log(filteredDevelopers);
+              }
+            })
+          })
         });
         console.log("Filtrati", filteredDevelopers);
         return filteredDevelopers;
@@ -89,16 +108,23 @@ export default {
     <label for="technologies">{{ technology.name }}</label>
   </div>
   <Filters @filterByVote="getFilterVote" />
+  <filterComment @filterByComment="getFilterComment" />
 
   <div class="container">
     <h1 class="title text-center my-4 fw-bold">I nostri programmatori</h1>
-    <div class="row">
-      <!-- <div v-if="developersByVote === ''" class="col" v-for="developer in developers" :key="developer.id">
-        <DeveloperCard :developer="developer" />
-      </div> -->
-      <div class="col" v-for="developer in filteredDevelopers" :key="developer.id">
-        <DeveloperCard :developer="developer" />
+    <div v-if="filteredDevelopers.length !== 0">
+    
+      <div class="row">
+        <!-- <div v-if="developersByVote === ''" class="col" v-for="developer in developers" :key="developer.id">
+          <DeveloperCard :developer="developer" />
+        </div> -->
+        <div class="col" v-for="developer in filteredDevelopers" :key="developer.id">
+          <DeveloperCard :developer="developer" />
+        </div>
       </div>
+    </div>
+    <div v-else>
+      <p class="text-center">non ci sono programmatori relativi a questa ricerca</p>
     </div>
   </div>
 </template>
