69a2f966b02254fb22269ed0c2b2293f16f6ee55
nespolisimmaco <snespoli@yahoo.com>
2023-07-11 11:15:04 +0200

    Created Filter for search bar

--- src/components/Filters.vue
+++ src/components/Filters.vue
@@ -1,13 +1,12 @@
 <script>
 import axios from 'axios';
-import{store} from "../store";
+import { store } from "../store";
 
-export default{
+export default {
     name: 'Filters',
-    emits:[filterVote],
-    data(){
-        return{
-            selectedVote:'',
+    data() {
+        return {
+            selectedVote: '',
             store,
         }
     },
@@ -15,17 +14,14 @@ export default{
 </script>
 
 <template>
-   <select name="vote" v-model="selectedVote" id="" @change="$emit('filterVote', selectedVote)">
-    <option value="">All</option>
-    <option value="1">1</option>
-    <option value="2">2</option>
-    <option value="3">3</option>
-    <option value="4">4</option>
-    <option value="5">5</option>
-   </select> 
-
+    <select name="vote" v-model="selectedVote" id="vote" @change="$emit('filterByVote', selectedVote)">
+        <option value="">All</option>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+    </select>
 </template>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -3,23 +3,21 @@ import axios from 'axios';
 import { store } from "../store";
 import DeveloperCard from "./DeveloperCard.vue";
 import Filters from './Filters.vue';
+
 export default {
   name: "SearchBar",
   components: {
     DeveloperCard,
-    Filters
+    Filters,
   },
   data() {
     return {
       store,
-      technologies: [],
-      developers: [],
-      developerSelected: [],
-      ids: [],
-      idsString: "",
-      developerVote:[],
-      filteredDevelopers:[]
-      // paramsToString: ""
+      technologies: [], // Array di linguaggi
+      developers: [],  // Array di developer
+      developersByVote: [], // Developers filtrati in base alla media voto (>=)
+      ids: [],  // Array di id
+      idsString: "",  // Sringa con id collegati con &
     };
   },
   mounted() {
@@ -31,9 +29,7 @@ export default {
         this.technologies = resp.data.results;
       });
     },
-
     getDeveloper(id) {
-
       if (this.ids.includes(id)) {
         const index = this.ids.indexOf(id);
         this.ids.splice(index, 1);
@@ -50,35 +46,57 @@ export default {
         this.developers = (resp.data.results);
       })
     },
-    getFilterVote(vote){
-      axios.get(`${this.store.apiUrl}/api/reviews/` + vote)
-      .then((resp)=>{
-          console.log(resp);
-          this.developersVote = resp.data.results
-      })
+    getFilterVote(vote) {
+      console.log("Voto", vote);
+      // Non funziona bene
+      if (vote != '') {
+        axios.get(`${this.store.apiUrl}/api/reviews/` + vote)
+          .then((resp) => {
+            this.developersByVote = resp.data.results;
+            console.log("By vote", this.developersByVote);
+          })
+      } else {
+        this.developersByVote = this.developers;
+      }
     },
-
-    developerFilter(){
-
-    }
   },
+  computed: {
+    filteredDevelopers() {
+      // Non funziona bene
+      if (this.developersByVote != '') {
+        let filteredDevelopers = [];
+        this.developersByVote.forEach(elem => {
+          this.developers.forEach(dev => {
+            if (dev.id === elem.id) {
+              filteredDevelopers.push(dev);
+              console.log(filteredDevelopers);
+            }
+          });
+        });
+        console.log("Filtrati", filteredDevelopers);
+        return filteredDevelopers;
+      } else {
+        return this.developers;
+      }
+    }
+  }
 };
 </script>
 
 <template>
-  <div v-for="technology,index in technologies" :key="index">
+  <div v-for="technology in technologies" :key="technology.id">
     <input @change="getDeveloper(technology.id)" id="technology" type="checkbox" :value="technology.id" />
     <label for="technologies">{{ technology.name }}</label>
   </div>
-  <Filters @filterVote ="getFilterVote(selectedVote)"/>
-  <!-- <div v-for="developer in developers">
-    <DeveloperCard :developer="developer" />
-  </div> -->
+  <Filters @filterByVote="getFilterVote" />
 
   <div class="container">
     <h1 class="title text-center my-4 fw-bold">I nostri programmatori</h1>
     <div class="row">
-      <div class="col" v-for="developer,index in filteredDevelopers" :key="index">
+      <!-- <div v-if="developersByVote === ''" class="col" v-for="developer in developers" :key="developer.id">
+        <DeveloperCard :developer="developer" />
+      </div> -->
+      <div class="col" v-for="developer in filteredDevelopers" :key="developer.id">
         <DeveloperCard :developer="developer" />
       </div>
     </div>
