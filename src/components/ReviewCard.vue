4ed56a0e9781cf5d139750ba68b2f8ae7f338cd2
FrancescaLoria <francesca.loria90@hotmail.it>
2023-07-13 09:49:39 +0200

    stars

--- src/components/ReviewCard.vue
+++ src/components/ReviewCard.vue
@@ -3,8 +3,8 @@
         <div class="card my-4">
             <div class="card-body">
                 <h5 class="card-title">Cliente: {{ review.name }}</h5>
-                <p class="card-text">Voto: <i v-for="(star, index) in review.vote" :key="index"
-                        class="fa-solid fa-star"></i></p>
+                <p class="card-text">Voto: <i v-for="(star, index) in review.vote" :key="index" class="fa-solid fa-star"
+                        style="color: #efcd25"></i></p>
                 <div>Commento:</div>
                 <p class="card-text">{{ review.comment }}</p>
             </div>
@@ -27,5 +27,6 @@ export default {
 <style lang="scss" scoped>
 .fa-star {
     color: yellow;
+
 }
 </style>
