1bf8a94a0169349118da50717f2f65593b670312
FrancescaLoria <francesca.loria90@hotmail.it>
2023-07-13 23:52:37 +0200

    vote-and-review

--- src/components/DeveloperCard.vue
+++ src/components/DeveloperCard.vue
@@ -37,40 +37,51 @@ export default {
     <div class="container ms_container">
         <div class="col">
             <div class="card developer-card">
-                <div class="container-image ">
-                    <!-- SEZIONE IMMAGINE -->
-                    <div class="container-img">
-                        <img v-if="developer.photo" :src="`${store.apiUrl}/storage/${developer.photo}`" alt="">
-                        <img v-else src="../assets/image/webdeveloper.jpg" alt="">
-                    </div>
-
-                </div>
-
-                <div class="name-vote-tech">
-                    <div class="container-name d-flex justify-content-center">
-                        <!-- SEZIONE NOME -->
-                        <h2 class="title">{{ developer.name }}</h2>
-                        <h2 class="subtitle ms-1">{{ developer.surname }}</h2>
-                    </div>
-                    <div class="container-media d-flex justify-content-center">
-                        <!-- SEZIONE MEDIA VOTO E RECENSIONE -->
-                        <div class="vote">VOTO</div>
-                        <div class="review ms-1">RECENSIONE</div>
-                    </div>
-
-
-
-                    <!-- SEZIONE TECNOLOGIE -->
-                    <div class="tech text-center my-2">
-                        <h4>TECNOLOGIE</h4>
-                        <div class="cards-container d-flex gap-2 justify-content-center">
-                            <div class="card-text" v-for="item, index in developer.technologies" :key="index">
-                                <div>{{ item.name }}</div>
-                            </div>
+                <div class="cont-card">
+                    <div class="container-image ">
+                        <!-- SEZIONE IMMAGINE -->
+                        <div class="container-img">
+                            <img v-if="developer.photo" :src="`${store.apiUrl}/storage/${developer.photo}`" alt="">
+                            <img v-else src="../assets/image/webdeveloper.jpg" alt="">
                         </div>
 
                     </div>
+
+                    <div class="name-vote-tech">
+                        <div class="container-name d-flex justify-content-center">
+                            <!-- SEZIONE NOME -->
+                            <h2 class="title">{{ developer.name }}</h2>
+                            <h2 class="subtitle ms-1">{{ developer.surname }}</h2>
+                        </div>
+                        <div class="container-media text-center ">
+                            <!-- SEZIONE MEDIA VOTO E RECENSIONE -->
+                            <div class="vote fst-italic fw-bold">MEDIA VOTO : <span class="text-primary fw-bold">{{
+                                developer.avg_vote
+                            }}</span></div>
+                            <div class="review fst-italic fw-bold">NUMERO RECENSIONI : <span class="text-primary fw-bold">{{
+                                developer.total_review }}</span></div>
+                        </div>
+
+
+
+                        <!-- SEZIONE TECNOLOGIE -->
+                        <div class="tech text-center my-2">
+                            <h4>TECNOLOGIE</h4>
+                            <div class="cards-container d-flex gap-2 justify-content-center fst-italic fw-bold">
+                                <div class="card-text" v-for="item, index in developer.technologies" :key="index">
+                                    <div>{{ item.name }}</div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
                 </div>
+
+                <!-- BOTTONE -->
+                <button type="button" class="btn fw-bold my-2">
+                    <router-link :to="{ name: 'developerDetails', params: { id: developer.id } }"
+                        class=" card-link">DETAILS</router-link>
+                </button>
 
             </div>
         </div>
@@ -109,12 +120,19 @@ export default {
     .btn {
         background-color: $pink;
         color: $light-green;
+        width: 20%;
+        margin: 0 auto;
+
+        a {
+            text-decoration: none;
+            color: unset;
+        }
 
     }
 }
 
 @media screen and (min-width: 768px) {
-    .developer-card {
+    .cont-card {
         display: flex;
         flex-direction: row;
         align-items: center;
