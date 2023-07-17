44de146322ed56e6c9c36f9c574494b84b525ede
FrancescaLoria <francesca.loria90@hotmail.it>
2023-07-17 10:33:46 +0200

    review card main

--- src/components/DeveloperCard.vue
+++ src/components/DeveloperCard.vue
@@ -27,55 +27,46 @@ export default {
             </div>
         </div>
 
-        <div class="container-image ">
-            <!-- SEZIONE IMMAGINE -->
-            <div class="container-img">
-                <img v-if="developer.photo" :src="`${store.apiUrl}/storage/${developer.photo}`" alt="">
-                <img v-else src="../assets/image/webdeveloper.jpg" alt="">
+        <div class="name-vote-tech">
+            <div class="container-name d-flex justify-content-center">
+                <!-- SEZIONE NOME -->
+                <h2 class="title">{{ developer.name }}</h2>
+                <h2 class="subtitle ms-1">{{ developer.surname }}</h2>
             </div>
-
-        </div>
-    </div>
-
-    <div class="name-vote-tech">
-        <div class="container-name d-flex justify-content-center">
-            <!-- SEZIONE NOME -->
-            <h2 class="title">{{ developer.name }}</h2>
-            <h2 class="subtitle ms-1">{{ developer.surname }}</h2>
-        </div>
-        <div class="container-media text-center ">
-            <!-- SEZIONE MEDIA VOTO E RECENSIONE -->
-            <div class="vote fst-italic fw-bold">MEDIA VOTO :
-                <span v-for="vote, id in developer.average_vote" :key="id">
-                    <i class="fa-solid fa-star" style="color: rgb(239, 205, 37);"></i>
-                </span>
-            </div>
-            <div class="review fst-italic fw-bold">NUMERO RECENSIONI : <span class="text-primary fw-bold">{{
-                developer.total_review }}</span></div>
-        </div>
-
-
-
-        <!-- SEZIONE TECNOLOGIE -->
-        <div class="tech text-center my-2">
-            <h4>TECNOLOGIE</h4>
-            <div class="cards-container d-flex flex-wrap gap-2 justify-content-center fst-italic fw-bold">
-                <div class="card-text" v-for="item, index in developer.technologies" :key="index">
-                    <div>{{ item.name }}</div>
+            <div class="container-media text-center ">
+                <!-- SEZIONE MEDIA VOTO E RECENSIONE -->
+                <div class="vote fst-italic fw-bold">MEDIA VOTO :
+                    <span v-for="vote, id in developer.average_vote" :key="id">
+                        <i class="fa-solid fa-star" style="color: rgb(239, 205, 37);"></i>
+                    </span>
                 </div>
+                <div class="review fst-italic fw-bold">NUMERO RECENSIONI : <span class="text-primary fw-bold">{{
+                    developer.total_review }}</span></div>
             </div>
 
+
+
+            <!-- SEZIONE TECNOLOGIE -->
+            <div class="tech text-center my-2">
+                <h4>TECNOLOGIE</h4>
+                <div class="cards-container d-flex flex-wrap gap-2 justify-content-center fst-italic fw-bold">
+                    <div class="card-text" v-for="item, index in developer.technologies" :key="index">
+                        <div>{{ item.name }}</div>
+                    </div>
+                </div>
+
+            </div>
         </div>
-    </div>
 
 
-    <!-- BOTTONE -->
-    <div class="cont-btn text-center">
+        <!-- BOTTONE -->
+        <div class="cont-btn text-center">
 
-        <button type="button" class="btn fw-bold text-dark my-2">
-            <router-link :to="{ name: 'developerDetails', params: { id: developer.id } }"
-                class=" card-link">DETAILS</router-link>
-        </button>
+            <button type="button" class="btn fw-bold text-dark my-2">
+                <router-link :to="{ name: 'developerDetails', params: { id: developer.id } }"
+                    class=" card-link">DETAILS</router-link>
+            </button>
+        </div>
     </div>
 </template>
 
